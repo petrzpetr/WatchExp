6801b43225169004d8b5d6384fa7dcaf4b69f91c
Robert Germic <germic@skycom.com>
2020-07-19 21:14:59 +0200

    pridal button a alert

--- exp01/exp01 WatchKit Extension/ContentView.swift
+++ exp01/exp01 WatchKit Extension/ContentView.swift
@@ -8,8 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var showAlert = false
+    
     var body: some View {
-        Text("Hello, World 01!").padding()
+        VStack {
+            HStack {
+                Text("Nahore 1").padding()
+                Text("Nahore 2").padding()
+            }
+            Button(action: {
+                self.showAlert = true
+            }) {
+                Text("Dole knoflik")
+                    .padding()
+                    .foregroundColor(.red)
+            }
+            .alert(isPresented: $showAlert) {
+                Alert(title: Text("Bacha!!!"),
+                      message: Text("Jajže bože strach veliky! Padli Turci na Poniky!"))
+            }
+        }
     }
 }
 
